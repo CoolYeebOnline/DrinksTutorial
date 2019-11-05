aadde1886cc1d3fb3dae57242f551897c9005c06
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2019-11-05 17:14:16 +0000

    Drink Tutorial from a while ago, just brushing up on it ok <3 love you will take care now

---
+++ DrinkMachine/DrinkMachine/DrinkMachine.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+
+int main()
+{
+	int drinkChoice;
+
+	switch (drinkChoice) {
+	case 1:
+		std::cout << "Cola-Type Drink" << std::endl;
+		break;
+
+	case 2:
+		std::cout << "79p Sugerade" << std::endl;
+		break;
+
+	case 3:
+		std::cout << "Mayoghurt" << std::endl;
+		break;
+
+
+	}
+
+
+
+
+
+
+
+
+
+}
+
